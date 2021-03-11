a1981e8241c33e2dc85c159dd06f0f2428bb32e1
jamesd <jamesd@briefyourmarket.co.uk>
2021-03-11 14:13:47 +0000

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => {
+  return {
+    root: {
+      backgroundColor: 'black',
+      width: '100vw',
+      height: '141vw',
+      margin:0,
+    }
+  }
+});
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+
     </div>
   );
 }
